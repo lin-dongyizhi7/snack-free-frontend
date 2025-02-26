580113553b406adcd5d4a1403643197cf818e92d
lin-dongyizhi7 <2985956026@qq.com>
2025-02-27 00:05:59 +0800

    配置

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,15 @@
 <template>
-    <div></div>
+  <div class="p-12">
+    <div class="header"></div>
+    <div class="body">
+        <calender-record></calender-record>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
- 
+import CalenderRecord from "../calender/index.vue";
+
 </script>
 
-<style scoped lang="less">
-</style>
+<style scoped lang="less"></style>
